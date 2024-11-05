8822049147d6791b18f4cbd901e90cecfcb1508f
UltimateQuack <patoflamejantetv@gmail.com>
2024-11-05 16:44:38 -0300

    Changed to Pokemons -> Quotes

--- generate.py
+++ generate.py
@@ -1,13 +1,14 @@
 import requests, json, random
 
-f = open("./README.md", "w")
-pokemon_id = random.randint(1, 1025)
-res = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}')
-result = json.loads(res.text)
-f.write(f'''<p align="center">
-    <img src="{result['sprites']['front_default']}" width="150" height="150">
-</p>
-<h3 align="center">You have been greeted by - <b>{result['name'].title()}</b></h3>
-<h3 align="center">Have a wonderful day!</h3>
-''')
-f.close()
+# Fetch a random quote from the quote API
+quote_res = requests.get("https://zenquotes.io/api/random")
+quote_data = json.loads(quote_res.text)
+random_quote = quote_data[0]['q']  # Quote text
+quote_author = quote_data[0]['a']  # Quote author
+
+# Write output to README.md
+with open("./README.md", "w") as f:
+    f.write(f'''<h3 align="center">You have been greeted by - <b>{quote_author}</b></h3>
+    <h4 align="center">"{random_quote}" - <i>{quote_author}</i></h4>
+    <h3 align="center">Have a wonderful day!</h3>
+    ''')
